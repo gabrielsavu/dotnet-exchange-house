814a786e8e32c68912e24a84e29816951d81a673
Savu Liviu <gabriel.savu99@gmail.com>
2021-01-09 23:27:33 +0200

    Login front end

---
+++ ExchangeOffice/ClientApp/src/helpers/AuthHeader.js
@@ -0,0 +1,10 @@
+﻿import { AuthenticationService } from './services';
+
+export function authHeader() {
+    const currentUser = AuthenticationService.currentUserValue;
+    if (currentUser && currentUser.token) {
+        return { Authorization: `Bearer ${currentUser.token.accessToken}` };
+    } else {
+        return {};
+    }
+}
