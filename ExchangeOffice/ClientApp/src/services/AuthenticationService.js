9c6cdad13cf31738b83e89f9d6231f9f782a32f8
Savu Liviu <gabriel.savu99@gmail.com>
2021-01-10 03:28:50 +0200

    User front end

---
+++ ExchangeOffice/ClientApp/src/services/AuthenticationService.js
@@ -0,0 +1,38 @@
+﻿import {BehaviorSubject} from 'rxjs';
+import {HandleResponse} from '../helpers';
+
+const currentUserSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('currentUser')));
+
+export const AuthenticationService = {
+    login,
+    logout,
+    currentUser: currentUserSubject.asObservable(),
+    get currentUserValue () { return currentUserSubject.value }
+};
+
+function login(email, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    };
+
+    return fetch(`api/Authentication/Login`, requestOptions)
+        .then(HandleResponse)
+        .then(response => {
+
+            localStorage.setItem('currentUser', JSON.stringify(response));
+            currentUserSubject.next(response);
+
+            return response;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    currentUserSubject.next(null);
+}
---
+++ ExchangeOffice/ClientApp/src/services/UserService.js
@@ -0,0 +1,10 @@
+﻿import {AuthHeader, HandleResponse} from '../helpers';
+
+export const UserService = {
+    getAll
+};
+
+function getAll() {
+    const requestOptions = {method: 'GET', headers: AuthHeader()};
+    return fetch(`user`, requestOptions).then(HandleResponse);
+}
