fd5753804704790a3b879f00a52e483c62298ea7
Savu Liviu <gabriel.savu99@gmail.com>
2021-01-11 21:09:18 +0200

    Ready for Azure deployment

---
+++ publish/ClientApp/build/precache-manifest.59d3f4dd266d6a63b4697b2c46560bd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d156da6344586649a42503a6602f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "657b91b78598b13dabac",
+    "url": "/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "6ae4b8f5643941136b81",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "657b91b78598b13dabac",
+    "url": "/static/js/2.76a03bd7.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/static/js/2.76a03bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae4b8f5643941136b81",
+    "url": "/static/js/main.63eba1c9.chunk.js"
+  },
+  {
+    "revision": "a977f65c8209cec9bfb5",
+    "url": "/static/js/runtime-main.bf37c43d.js"
+  }
+]);
